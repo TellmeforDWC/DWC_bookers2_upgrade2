7e29ea7b953e15e282bbe668f7e221f8f008b724
TellmeforDWC <rikugame1003@gmail.com>
2021-10-06 11:51:12 +0000

    [finish]bookers2 フォロー機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'relationships/show_follow'
+  get 'relationships/show_follower'
   devise_for :users
 
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get "followees" => "relationships#show_followees", as: "show_followees"
+    get "followers" => "relationships#show_followers", as: "show_followers"
+  end
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
